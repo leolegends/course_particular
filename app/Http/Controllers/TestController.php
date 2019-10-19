14f47acdf9a3a04780867992f943fb8f437107ee
Leonardo Ribeiro <lviniciusribeiro@yahoo.com.br>
2019-10-19 11:46:49 -0300

    Conditions Hopes

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -20,16 +20,45 @@ class TestController extends Controller
 
 
             $ocorrencias = [
-                "",                
                 "ERRO",
                 "CHAMADO",
                 "RECLAMACAO",
                 "FINANCEIRO"
                 ];
 
-                foreach($ocorrencias as $ocorrencia){
-                    echo $ocorrencia . "<br>";     
-                    }
+            $quantidade_de_ocorrencias = count($ocorrencias);
+
+            
+
+
+
+            
+                //TODO: Foreach, For, While. 
+
+                //? While
+                //TODO: While significa Enquanto;
+
+                // $a = 0;
+
+                // while($a < $quantidade_de_ocorrencias){
+                //     // var_dump($a);
+                //     var_dump($ocorrencias[$a]);
+                //     $a++;
+                // }
+
+                //? For
+               //*      1      2       3
+                // for($i = $quantidade_de_ocorrencias - 1; $i >= 0; $i--){
+                //     var_dump($ocorrencias[$i]);
+                //     // var_dump($i);
+                // }
+
+
+                //? Foreach
+                // foreach($ocorrencias as $ocorrencia){
+                //     // echo $ocorrencia . "<br>";
+                //     var_dump($ocorrencia);     
+                // }
                    
 
        //dd($ocorrencias[0]);
